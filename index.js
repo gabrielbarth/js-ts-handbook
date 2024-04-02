b086ebaefb6688352cddc593bd68c9d38e0636d5
gabrielbarth <gabriel.silverio@stone.com.br>
2024-04-01 23:42:49 -0400

    first commit: initializes handbook node project

---
+++ index.js
@@ -0,0 +1,12 @@
+const welcomeMessage = `
+ Welcome to Gabriel Barth's typescript and javascript handbook. 
+ Here I have taken some notes about code. 
+ \n You can choose a javascript file to execute using node to take a look.
+ Just type it: node javacript/<chosen-file-name> 
+ \n In case of typescript, remember to turn it into Javascript before.
+ Run: npx tsc
+ Then just choose a file: node typescript/dist/<chosen-file-name>
+ \n\n I hope you enjouy it :D.
+`;
+
+console.log(welcomeMessage);
